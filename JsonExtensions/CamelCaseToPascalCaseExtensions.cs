6e4a2e3c0a28f24c37c287a9edbf76e5540e85a8
Higgins Mwinamu Mugaisi <Higgins.Mwinamu@segen.co.uk>
2022-04-28 10:15:25 +0300

    Add project files.

---
+++ JsonExtensions/CamelCaseToPascalCaseExtensions.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace JsonExtensions
+{
+    public static class CamelCaseToPascalCaseExtensions
+    {
+        public static void Capitalize(this JArray jArr)
+        {
+            foreach (var x in jArr.ToList())
+            {
+                if (x is JObject childObj)
+                {
+                    childObj.Capitalize();
+                    continue;
+                }
+                if (x is JArray childArr)
+                {
+                    childArr.Capitalize();
+                }
+            }
+        }
+
+        public static void Capitalize(this JObject jObj)
+        {
+            foreach (var kvp in jObj.Cast<KeyValuePair<string, JToken>>().ToList())
+            {
+                jObj.Remove(kvp.Key);
+                var newKey = kvp.Key.Capitalize();
+                if (kvp.Value is JObject childObj)
+                {
+                    childObj.Capitalize();
+                    jObj.Add(newKey, childObj);
+                    return;
+                }
+
+                if (kvp.Value is JArray childArr)
+                {
+                    childArr.Capitalize();
+                    jObj.Add(newKey, childArr);
+                    return;
+                }
+                jObj.Add(newKey, kvp.Value);
+            }
+        }
+
+        public static string Capitalize(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                throw new ArgumentException("empty string");
+            }
+            char[] arr = str.ToCharArray();
+            arr[0] = char.ToUpper(arr[0]);
+            return new string(arr);
+        }
+    }
+}
---
+++ JsonExtensions/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using JsonExtensions;
+using Newtonsoft.Json.Linq;
+
+string jsonString =
+    "{\"personName\":{\"firstName\":\"Emma\",\"lastName\":\"Watson\"}}";
+var jObj = JObject.Parse(jsonString);
+jObj.Capitalize();
+Console.WriteLine(jObj.ToString()); //yay!
